1198197af1b1b26da8d24d5be385e2fc5b6f18ae
johnclamy <johnclamy@gmail.com>
2026-08-10 11:07:31 +0100

    make the helper func file indexable

--- frontend/src/helper/index.ts
+++ frontend/src/helper/index.ts
@@ -1,16 +1,21 @@
-/************************
- *   Helper functions   *
- *                      *
- ************************/
+/****************************************************
+ *   HELPER FUNCTIONS                               *
+ *                                                  *
+ *   (01)  capitalizeFirstLetter                    *
+ *                                                  *
+ *                                                  *
+ ****************************************************/
 
 
-// Capitalize the first Character of a given string
+// (01) Capitalize the first Character of a given string
 
-function capitalizeFirstLetter<T extends string>(str: T): Capitalize<T>;
+function capitalizeFirstLetter<T extends string>(str: T): Capitalize<T>
 function capitalizeFirstLetter(str: string): string {
     if (!str) return str
     return str.charAt(0).toUpperCase() + str.slice(1)
 }
 
 
-export { capitalizeFirstLetter }
+export {
+    capitalizeFirstLetter,
+}
