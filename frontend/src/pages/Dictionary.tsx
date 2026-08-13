a427f3bc9ac03ddcaf9a96ad9bfb085212689dea
johnclamy <johnclamy@gmail.com>
2026-08-13 22:24:22 +0100

    replace projects page with Dictionary page component

---
+++ frontend/src/pages/Dictionary.tsx
@@ -0,0 +1,4 @@
+const Dictionary: React.FC = () => <h1>The Dictionary page</h1>
+
+
+export default Dictionary
--- frontend/src/pages/Project.tsx
+++
@@ -1,4 +0,0 @@
-const Project: React.FC = () => <h1>The Projects page</h1>
-
-
-export default Project
