d7b1620271efe828ddf5c3af044d1603e7af35d6
johnclamy <johnclamy@gmail.com>
2026-08-13 22:26:10 +0100

    add routes for new and amended pages components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,20 +1,22 @@
 import { BrowserRouter, Routes, Route } from 'react-router'
 import Layout from './components/Layout'
+import Default from './pages/Default'
 import Employee from './pages/Employee'
 import Customer from './pages/Customer'
-import Project from './pages/Project'
+import Dictionary from './pages/Dictionary'
 import Calendar from './pages/Calendar'
 
 
 const App: React.FC = () => (
     <BrowserRouter>
         <Routes>
-            <Route path="/employee" element={<Layout />}>
-                <Route index element={<Employee />} />
-                <Route path="customer" element={<Customer />} />
-                <Route path="project" element={<Project />} />
-                <Route path="calendar" element={<Calendar />} />
+            <Route path="/" element={<Layout />}>
+                <Route index element={<Default />} />
             </Route>
+            <Route path="employee" element={<Employee />} />
+            <Route path="customer" element={<Customer />} />
+            <Route path="dictionary" element={<Dictionary />} />
+            <Route path="calendar" element={<Calendar />} />
         </Routes>
     </BrowserRouter> 
 )
