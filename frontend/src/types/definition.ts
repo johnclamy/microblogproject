2ae5330a5db2efb5198f0a983d4bb3f950a0ce09
johnclamy <johnclamy@gmail.com>
2026-08-16 12:22:57 +0100

    remove the any type from  the DefinitionData interface

--- frontend/src/types/definition.ts
+++ frontend/src/types/definition.ts
@@ -1,6 +1,30 @@
+export interface Definition {
+    definition: string
+    example?: string
+    synonyms?: string[]
+    antonyms?: string[]
+}
+
+
+export interface Meaning {
+    partOfSpeech: string
+    definitions: Definition[]
+    synonyms?: string[]
+    antonyms?: string[]
+}
+
+
+export interface Phonetic {
+    text?: string
+    audio?: string
+    sourceUrl?: string
+}
+
+
 export default interface DefinitionData {
-    word?: string
-    definition?: string
-    meanings: any[]
-    phonetic?: any[]
+    word: string
+    phonetic?: string
+    phonetics?: Phonetic[]
+    meanings: Meaning[]
+    sourceUrls?: string[]
 }
