5782fc17909451d5542fac69778a6c0c9b69bedb
johnclamy <johnclamy@gmail.com>
2026-08-16 19:36:55 +0100

    redirect url query with the use of navigate Hook

--- frontend/src/pages/Dictionary.tsx
+++ frontend/src/pages/Dictionary.tsx
@@ -1,4 +1,27 @@
-const Dictionary: React.FC = () => <h1>The Dictionary page</h1>
+import { useState, useEffect } from 'react'
+import { useNavigate } from 'react-router'
+
+
+const Dictionary: React.FC = () => {
+    const [word, setWord] = useState<string>('')
+    const navigate = useNavigate()
+ 
+    return (
+        <div>
+            <input
+                type="text"
+                onChange={(e) => setWord(e.target.value)}
+            />
+            <button
+                onClick={() => {
+                    navigate('/definition/' + word.trim(), {replace:true})
+                }}
+            >
+                Search
+            </button>
+        </div>
+    )
+}
 
 
 export default Dictionary
