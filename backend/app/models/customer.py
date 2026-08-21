9efd9ddd881edc9af1c643d024910fc2fd7b1d3c
johnclamy <johnclamy@gmail.com>
2026-08-21 16:02:12 +0100

    replace the @dataclass with a SQLAlchemy ORM class that inherits from Base

--- backend/app/models/customer.py
+++ backend/app/models/customer.py
@@ -1,10 +1,13 @@
 # app/models/customer.py
-from dataclasses import dataclass
+from sqlalchemy import Column, Integer, String
+from app.db.database import Base
 
 
-@dataclass
-class Customer:
-    username: str
-    name: str
-    email: str
-    industry: str
+class Customer(Base):
+    __tablename__ = "customers"
+
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String, unique=True, index=True, nullable=False)
+    name = Column(String, nullable=False)
+    email = Column(String, unique=True, index=True, nullable=False)
+    industry = Column(String, nullable=False)
